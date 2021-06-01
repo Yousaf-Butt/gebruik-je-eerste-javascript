984166230d22fbbeaa2b6e26b0661cee1a504acc
Yousaf <78146910+Yousaf-Butt@users.noreply.github.com>
2021-06-01 11:28:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let name = "Yousaf";
+console.log(name);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(8 - 2);
+console.log(5 * 5);
+console.log(6 / 3);
+console.log(7 % 4);
+let age = 41;
+console.log(typeof age);
+age = "41";
+console.log(typeof age);
